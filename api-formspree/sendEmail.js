a62f9b2739bcb08b112db5943e34d9c02d55d002
isacko27 <67716378+isacko27@users.noreply.github.com>
2023-12-06 21:48:56 -0600

    form submision

---
+++ api-formspree/sendEmail.js
@@ -0,0 +1,34 @@
+var form = document.getElementById("cform");
+var successMessage = document.querySelector(".alert-success");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("cform-status");
+  var data = new FormData(event.target);
+
+  try {
+    var response = await fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      form.reset();
+      successMessage.style.display = "block"; // Mostrar el mensaje de éxito
+    } else {
+      var responseData = await response.json();
+      if (Object.hasOwnProperty.call(responseData, 'errors')) {
+        status.innerHTML = responseData.errors.map(error => error.message).join(", ");
+      } else {
+        status.innerHTML = "Oops! There was a problem submitting your form";
+      }
+    }
+  } catch (error) {
+    status.innerHTML = "Oops! There was a problem submitting your form";
+  }
+}
+
+form.addEventListener("submit", handleSubmit);
